93ddb27d5ed08f9888d43246eb077f7953459d61
David Tahim <davidtahim@gmail.com>
2024-08-19 21:28:26 -0300

    adicionei a figura da imagem de fundo do jogo

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
+// import HelloWorldScene from './HelloWorldScene'
+import Game from './Game'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	parent: 'app',
 	width: 800,
-	height: 600,
+	height: 640,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 },
 		},
 	},
-	scene: [HelloWorldScene],
+	// scene: [HelloWorldScene], cena original
+	scene: [Game]
 }
 
 export default new Phaser.Game(config)
